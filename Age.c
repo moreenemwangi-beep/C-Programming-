b6d4b80216d91138375f1aafd5af02df3c582e24
moreenemwangi-beep <moreenemwangi@gmail.com>
2025-09-25 22:07:38 +0300

    Add files via upload

---
+++ Age.c
@@ -0,0 +1,17 @@
+/*
+Name: Moreene Ng'endo
+Reg no: CT100/G/26148/25
+Date: 25th sept 2025
+Description: Units 
+*/
+
+#include <stdio.h>
+int main(){
+	int age;
+	//user input
+	printf("enter your age, \n");
+	scanf("%d", & age);
+	printf("your entered age is: %d",age);
+	
+return 0;
+}
