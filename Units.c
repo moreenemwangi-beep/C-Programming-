66ccba49f4a35dedfda5087c293101f5acd50f94
moreenemwangi-beep <moreenemwangi@gmail.com>
2025-09-25 22:10:29 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,20 @@
+/*
+Name: Moreene Ng'endo
+Reg no: CT100/G/26148/25
+Date: 25th sept 2025
+Description: Units 
+*/
+
+#include <stdio.h>
+int main(){
+printf("unit for this semester:\n");
+printf("computer system and organisations\n");
+printf("fundamentals of ICT\n");
+printf("essentials of accounting\n");
+printf("analogue electronics\n");
+printf("introduction to computer application\n");
+printf("communication skills\n");
+printf("mathematic for science\n");
+printf("introduction toprogramming and algorithms\n");
+return 0 ;
+}
