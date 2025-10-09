f18909c4c3f4e16d5ce6884284df4d06026ef860
Mucheke <moreenemwangi@gmail.com>
2025-10-09 23:38:00 +0300

    Add files via upload

---
+++ number_loop.c
@@ -0,0 +1,18 @@
+/*
+Name :Moreene Ng'endo
+Reg No:26148
+Description:using for loop
+*/
+#include <stdio.h>
+
+int main() {
+//variable declaration 
+    int i;
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+
+    
